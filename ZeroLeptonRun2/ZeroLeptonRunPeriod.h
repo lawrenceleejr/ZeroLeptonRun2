27afc564f08054078a6e4c9b32159f6e8d2b4a6b
lduflot <lduflot@4525493e-7705-40b1-a816-d608a930855b>
2015-03-31 16:25:57 +0000

    add p13tev period
    git-svn-id: svn+ssh://svn.cern.ch/reps/atlasoff/PhysicsAnalysis/SUSYPhys/Factory/ZeroLeptonRun2/trunk@658227 4525493e-7705-40b1-a816-d608a930855b

--- ZeroLeptonRun2/ZeroLeptonRunPeriod.h
+++ ZeroLeptonRun2/ZeroLeptonRunPeriod.h
@@ -3,12 +3,13 @@
 
 #include <string>
 
-enum ZeroLeptonRunPeriod {p7tev,p8tev,INVALID};
+enum ZeroLeptonRunPeriod {p7tev,p8tev,p13tev,INVALID};
 
 inline ZeroLeptonRunPeriod periodFromString(const std::string& period)
 {
   if ( period == "p7tev" ) return p7tev;
   else if ( period == "p8tev" ) return p8tev;
+  else if ( period == "p13tev" ) return p13tev;
   return INVALID;
 }
 
