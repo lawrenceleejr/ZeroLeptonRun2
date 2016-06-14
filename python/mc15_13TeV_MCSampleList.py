5ecd0e661244c0cfa5f2e094daa86207ef0a04e0
Russell Smith <rsmith@cern.ch>
2016-06-14 09:57:49 -0400

    using the names instead of copying things a million times

--- python/mc15_13TeV_MCSampleList.py
+++ python/mc15_13TeV_MCSampleList.py
@@ -3,55 +3,55 @@ __doc__ = """List of samples to be used in the analysis"""
 ##
 ## Baseline samples   built from 50ns tags [r6630, r6647, r6655, r6767, r6793, r6802, r6828]
 ##
-#lZjets   = range(361444,361467+1) # Sherpa Znunu 
-#lZjets  += range(361372,361395+1) # Sherpa Zee
-#lZjets  += range(361396,361419+1) # Sherpa Zmumu
-#lZjets  += range(361420,361443+1) # Sherpa Ztautau
+#lZMassiveCB   = range(361444,361467+1) # Sherpa Znunu
+#lZMassiveCB  += range(361372,361395+1) # Sherpa Zee
+#lZMassiveCB  += range(361396,361419+1) # Sherpa Zmumu
+#lZMassiveCB  += range(361420,361443+1) # Sherpa Ztautau
 
-lZjets   = range(363412,363435+1) # Sherpa 2.2 Znunu 
-lZjets  += range(363391,363411+1) # Sherpa 2.2 Zee
-lZjets  += range(363364,363387+1) # Sherpa 2.2 Zmumu
-lZjets  += range(363102,363122+1) # + range(363361,363363+1) # Sherpa 2.2 Ztautau
-
-
-#lZjetsLO = range(407100,407183+1) # L0 Z+jets
-
-lYjets   = range(361039,361061+1) # Sherpa
-
-#lWjets   = range(361300,361323+1) # Sherpa Wenu missing pt<70
-#lWjets  += range(361324,361347+1) # Sherpa Wmunu  missing pt<70 
-#lWjets  += range(361348,361371+1) # Sherpa Wtaunu
-
-lWjets   = range(363463,363483+1) # Sherpa 2.2 Wenu missing pt<70
-lWjets  += range(363439,363459+1) # Sherpa 2.2 Wmunu  missing pt<70
-lWjets  += range(363334,363354+1) # Sherpa 2.2 Wtaunu  
+ZMassiveCB   = range(363412,363435+1) # Sherpa 2.2 Znunu
+ZMassiveCB  += range(363391,363411+1) # Sherpa 2.2 Zee
+ZMassiveCB  += range(363364,363387+1) # Sherpa 2.2 Zmumu
+ZMassiveCB  += range(363102,363122+1) # + range(363361,363363+1) # Sherpa 2.2 Ztautau
 
 
-lttbar   = [410000] # PowhegPythiaEvtGen_P2012_ttbar_hdamp172p5_nonallhad
-lsinglet = [410011, 410012, # PowhegPythia t-channel
+#ZMassiveCBLO = range(407100,407183+1) # L0 Z+jets
+
+GammaMassiveCB   = range(361039,361061+1) # Sherpa
+
+#Wjets   = range(361300,361323+1) # Sherpa Wenu missing pt<70
+#Wjets  += range(361324,361347+1) # Sherpa Wmunu  missing pt<70
+#Wjets  += range(361348,361371+1) # Sherpa Wtaunu
+
+Wjets   = range(363463,363483+1) # Sherpa 2.2 Wenu missing pt<70
+Wjets  += range(363439,363459+1) # Sherpa 2.2 Wmunu  missing pt<70
+Wjets  += range(363334,363354+1) # Sherpa 2.2 Wtaunu
+
+
+ttbar   = [410000] # PowhegPythiaEvtGen_P2012_ttbar_hdamp172p5_nonahad
+singlet = [410011, 410012, # PowhegPythia t-channel
             410013, 410014, # PowhegPythia Wt
             410025, 410026, # PowhegPythia s-channel
-            ]       
-lttbarX   = range(410066,410068+1)  # MadGraph5+Pythia ttW
-lttbarX  += range(410069,410075+1)  # MadGraph5+Pythia ttZ
-lttbarX  += [410081] # MadGraph5+Pythia ttWW
+            ]
+ttbarX   = range(410066,410068+1)  # MadGraph5+Pythia ttW
+ttbarX  += range(410069,410075+1)  # MadGraph5+Pythia ttZ
+ttbarX  += [410081] # MadGraph5+Pythia ttWW
 
-lTop     = lttbar + lsinglet + lttbarX
+Top     = ttbar + singlet + ttbarX
 
-# diboson samples
-#lDiBoson =  range(361063,361073+1)+[361077,]+range(361081,361089+1) 
-lDiBoson =  range(361063,361073+1)+[361077,]+range(361087,361088+1)+range(361091,361096+1) # use of 361087 is temporary to be replaced with 361097
+# diboson sampes
+#DibosonMassiveCB =  range(361063,361073+1)+[361077,]+range(361081,361089+1)
+DibosonMassiveCB =  range(361063,361073+1)+[361077,]+range(361087,361088+1)+range(361091,361096+1) # use of 361087 is temporary to be repaced with 361097
 
-lTriBoson = []
+TriBoson = []
 
-lZgamma  = [301908,301909] # Sherpa nunugamma 35<pt<140
-lWgamma  = [301893,301894,301902] # Sherpa  several missing samples
-lVgamma  = lZgamma + lWgamma
-lDiBoson = lDiBoson  + lTriBoson + lVgamma
+Zgamma  = [301908,301909] # Sherpa nunugamma 35<pt<140
+Wgamma  = [301893,301894,301902] # Sherpa  several missing sampes
+Vgamma  = Zgamma + Wgamma
+DibosonMassiveCB = DibosonMassiveCB  + TriBoson + Vgamma
 
-lQCDMC   = range(361020,361032+1) # Pythia8 dijet JZxW
+QCD   = range(361020,361032+1) # Pythia8 dijet JZxW
 
-lbaseline = lZjets + lWjets + lYjets + lTop + lQCDMC + lDiBoson
+baseline = ZMassiveCB + Wjets + GammaMassiveCB + Top + QCD + DibosonMassiveCB
 
 ##
 ## Alternative MC samples
@@ -59,46 +59,45 @@ lbaseline = lZjets + lWjets + lYjets + lTop + lQCDMC + lDiBoson
 
 ##[187150,187158+1]+[187160,187168+1]+[187170,187178+1]+[187180,187188+1]+[206618,206620+1] # diboson PowhegPythia
 
-lZjetsPowheg = [361106,361107,361108] # PowhegPythia8 samples
-lZjetsMadgraph = range(361500,361519+1)  # Madgraph+Pythia8
-lZtautaujetsSherpa22 = range(363102,363122+1) + range(363361,363363+1)
-lZmumujetsSherpa22 = range(363364,363387+1)
-lZeejetsSherpa22 = range(363388,363411+1)
-lZnunujetsSherpa22 = range(363412,363435+1)
-lZjetsSherpa22 = lZtautaujetsSherpa22+lZmumujetsSherpa22+lZeejetsSherpa22+lZnunujetsSherpa22
-lZjetsAlt = lZjetsPowheg + lZjetsMadgraph + lZjetsSherpa22
+ZMassiveCBPowheg = [361106,361107,361108] # PowhegPythia8 sampes
+ZMassiveCBMadgraph = range(361500,361519+1)  # Madgraph+Pythia8
+ZtautaujetsSherpa22 = range(363102,363122+1) + range(363361,363363+1)
+ZmumujetsSherpa22 = range(363364,363387+1)
+ZeejetsSherpa22 = range(363388,363411+1)
+ZnunujetsSherpa22 = range(363412,363435+1)
+ZMassiveCBSherpa22 = ZtautaujetsSherpa22+ZmumujetsSherpa22+ZeejetsSherpa22+ZnunujetsSherpa22
+ZMassiveCBAlt = lZMassiveCBPowheg + lZMassiveCBMadgraph + lZMassiveCBSherpa22
 
-lWjetsPowheg = [361100,361101,361102,361103,361104,361105]  # PowhegPythia8 samples
-lWjetsMadgraph = range(361520,361534+1)  # Madgraph+Pythia
-lWjtaunuetsSherpa22 = range(363331,363354+1) 
-lWmunujetsSherpa22 = range(363436,363459+1)
-lWenujetsSherpa22 = range(363460,363483+1)
-lWjetsSherpa22 = lWjtaunuetsSherpa22+lWmunujetsSherpa22+lWenujetsSherpa22
-lWjetsAlt = lWjetsPowheg + lWjetsMadgraph + lWjetsSherpa22
+WjetsPowheg = [361100,361101,361102,361103,361104,361105]  # PowhegPythia8 sampes
+WjetsMadgraph = range(361520,361534+1)  # Madgraph+Pythia
+WjtaunuetsSherpa22 = range(363331,363354+1)
+WmunujetsSherpa22 = range(363436,363459+1)
+WenujetsSherpa22 = range(363460,363483+1)
+WjetsSherpa22 = WjtaunuetsSherpa22+WmunujetsSherpa22+WenujetsSherpa22
+WjetsAlt = WjetsPowheg + WjetsMadgraph + WjetsSherpa22
 
-lttbarPowhegSyst = [410001,410002]
-lttbarMCatNLO = [410003]
-lttbarPowhegHerwing = [410004]
-lttbarPowhegP8 = [410006]
-lttbarSherpa = [410021,410022,410023]
-lttbarAlt = lttbarPowhegSyst + lttbarMCatNLO + lttbarPowhegHerwing + lttbarPowhegP8 + lttbarSherpa
-lsingletAlt = [ ]
+ttbarPowhegSyst = [410001,410002]
+ttbarMCatNLO = [410003]
+ttbarPowhegHerwing = [410004]
+ttbarPowhegP8 = [410006]
+ttbarSherpa = [410021,410022,410023]
+ttbarAlt = ttbarPowhegSyst + ttbarMCatNO + ttbarPowhegHerwing + ttbarPowhegP8 + ttbarSherpa
+singletAlt = [ ]
 
-lDiBosonPowheg =  range(361600,361611+1) 
-lDiBosonAlt = lDiBosonPowheg
+DibosonMassiveCBPowheg =  range(361600,361611+1)
+DibosonMassiveCBAlt = DibosonMassiveCBPowheg
 
 
-lalt = lZjetsAlt + lWjetsAlt + lttbarAlt + lsingletAlt + lDiBosonAlt
-
+alt = ZMassiveCBAlt + WjetsAlt + ttbarAlt + singletAlt + DibosonMassiveCBAlt
 
 ##
 ## truth level samples
 ##
 
-lZjetsTruth  = range(362000,362190+1) # Sherpa Z->nunu syst variations
-lZjetsTruth += range(361444,361467+1) # Sherpa Z->nunu
-lZjetsTruth += range(361515,361519+1) # Madgraph+Pythia8 Z->nunu
-lYjetsTruth = range(361039,361061+1) # Sherpa gamma+jets
+ZMassiveCBTruth  = range(362000,362190+1) # Sherpa Z->nunu syst variations
+ZMassiveCBTruth += range(361444,361467+1) # Sherpa Z->nunu
+ZMassiveCBTruth += range(361515,361519+1) # Madgraph+Pythia8 Z->nunu
+GammaMassiveCBTruth = range(361039,361061+1) # Sherpa gamma+jets
 
-ltruth = lZjetsTruth + lYjetsTruth
+truth = ZMassiveCBTruth + GammaMassiveCBTruth
 
